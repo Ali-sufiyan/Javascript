b04f26eed47c1a3a08e27b71e00f7c9e155c48d9
Ali-sufiyan <115947671+Ali-sufiyan@users.noreply.github.com>
2022-10-30 21:47:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,82 @@
+alert("web page is not available")
+window.alert("don't disturb me")
+var a=10;
+var b="20";
+var c= a + b;
+console.log(c);
+
+var a=20;
+var b=20;
+var c=50;
+var d= a - b + c
+console.log(d);
+
+var a=100;
+var b=10;
+var c=++a;
+var d= --b;
+console.log(d)
+
+var a=10;
+var b=10;
+ var c= ++a - --b + a + b - ++b - ++a;
+console.log(c);
+
+
+var a=20;
+var b=20;
+var c= a / b;
+console.log(c);
+
+
+
+var a=20;
+var b=20;
+var c= a * b;
+console.log(c);
+
+var firstname=("Ali"); 
+var lastname=("sufiyan");
+var fullname= firstname + lastname;
+console.log(fullname);
+
+
+var userName =("Ali") 
+ var message = ("Thanks");
+ var banger = ("!") ;
+ console.log(message + userName + banger);
+
+
+
+var intro= ("my");
+var firstname= ("Ali");
+var lastname= ("Sufiyan");
+var extra= ("HELLO");
+console.log(intro + firstname + lastname + extra);
+
+
+var a= 500;
+var b= 500;
+var c= 500;
+var d= 5000;
+var e= ++a - --a + --b - ++d + --d - d + a + b + c +d;
+console.log(e);
+
+var a= 10;
+var b= 10;
+var c= 10;
+var d= 10;
+var end= a * b - b / c + d + a;
+console.log(end);
+
+var fullname= ("enter your name")
+var email=("enter your email")
+var contact=("phone-number")
+console.log(fullname + email + contact)
+
+
+// var a=20;
+// var b=20;
+// var c= a - b;
+// alert("c");
+// console.log(a)
